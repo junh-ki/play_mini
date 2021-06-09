e2ab440c3455e94c48ebfcc560207146a31e8906
Junhyung Ki <kijoonh91@gmail.com>
2021-06-09 14:51:00 +0200

    getAllApartments REST endpoint added and tested

--- helloWorld/app/controllers/ApartmentRestController.java
+++ helloWorld/app/controllers/ApartmentRestController.java
@@ -1,4 +1,27 @@
 package controllers;
 
-public class ApartmentRestController {
+import models.Apartment;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.ApartmentService;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.List;
+
+public class ApartmentRestController extends Controller {
+
+    private final ApartmentService apartmentService;
+
+    @Inject
+    public ApartmentRestController(ApartmentService apartmentService) {
+        this.apartmentService = apartmentService;
+    }
+
+    public Result getApartments() {
+        List<Apartment> apartments = apartmentService.getAllApartments();
+        return ok(Json.toJson(apartments));
+    }
+
 }
