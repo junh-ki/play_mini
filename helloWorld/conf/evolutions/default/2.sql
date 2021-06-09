c88710671db86f46b08e6ef1e7d9f41c5af6160b
Junhyung Ki <kijoonh91@gmail.com>
2021-06-09 09:33:27 +0200

    sample dataset for h2 added

---
+++ helloWorld/conf/evolutions/default/2.sql
@@ -0,0 +1,16 @@
+# --- Sample dataset
+
+# --- !Ups
+
+insert into apartment (id, name, category, description, size, price) values ( 1, 'Munich Flat', 'A', 'Fancy Apartment', 32, 135.5 );
+insert into apartment (id, name, category, description, size, price) values ( 2, 'Stuttgart Flat', 'A', 'Fancy Apartment', 30, 120.5 );
+insert into apartment (id, name, category, description, size, price) values ( 3, 'Dortmund Flat', 'C', 'Economic Apartment', 31, 50.5 );
+insert into apartment (id, name, category, description, size, price) values ( 4, 'Berlin Flat', 'B', 'Middle-budget Apartment', 33, 93.5 );
+insert into apartment (id, name, category, description, size, price) values ( 5, 'Ulm Flat', 'C', 'Economic Apartment', 32, 64.5 );
+insert into apartment (id, name, category, description, size, price) values ( 6, 'Hamburg Flat', 'A', 'Fancy Apartment', 30, 127.5 );
+insert into apartment (id, name, category, description, size, price) values ( 7, 'Freiburg Flat', 'B', 'Middle-budget Apartment', 31, 100.5 );
+insert into apartment (id, name, category, description, size, price) values ( 8, 'Kiel Flat', 'C', 'Economic Apartment', 33, 70.5 );
+insert into apartment (id, name, category, description, size, price) values ( 9, 'Karlsruhe Flat', 'B', 'Middle-budget Apartment', 32, 110.5 );
+insert into apartment (id, name, category, description, size, price) values ( 10, 'Cologne Flat', 'A', 'Fancy Apartment', 30, 130.5 );
+insert into apartment (id, name, category, description, size, price) values ( 11, 'Dusseldorf Flat', 'A', 'Fancy Apartment', 31, 128.5 );
+insert into apartment (id, name, category, description, size, price) values ( 12, 'Bonn Flat', 'B', 'Middle-budget Apartment', 33, 103.5 );
