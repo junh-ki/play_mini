849942569434e19ad30b75e1f63883226145cc44
Junhyung Ki <kijoonh91@gmail.com>
2021-06-11 18:22:22 +0200

    feedback TODO lines added

--- helloWorld/app/controllers/ApartmentRestController.java
+++ helloWorld/app/controllers/ApartmentRestController.java
@@ -20,18 +20,20 @@ public class ApartmentRestController extends Controller {
     }
 
     public Result getApartments() {
+        // TODO: current page , page size | page-pagination
         List<Apartment> apartments = apartmentService.getAllApartments();
         return ok(Json.toJson(apartments));
     }
 
     public Result getApartment(Long id) {
         Apartment apartment = apartmentService.findApartmentById(id);
-        if (apartment == null) return badRequest("ID doesn't exist.");
+        if (apartment == null) return badRequest("ID doesn't exist."); // TODO: change the return status (400)
         return ok(Json.toJson(apartment));
     }
 
     public Result addApartment(String name, String category, String description, Integer size, Double price) {
         System.out.println("Category: " + category);
+        // TODO: separate method for validation
         if (!category.equals("A") && !category.equals("B") && !category.equals("C")) {
             return badRequest("Category should either be \"A\" or \"B\" or \"C\"!");
         } else if (size < 10) {
@@ -46,16 +48,21 @@ public class ApartmentRestController extends Controller {
 
     public Result deleteApartmentById(Long id) {
         Apartment apartment = apartmentService.deleteApartmentById(id);
-        if (apartment == null) return badRequest("ID doesn't exist.");
+        if (apartment == null) return badRequest("ID doesn't exist."); // change the return status (400)
         return ok(Json.toJson(apartment));
     }
 
     public Result updateApartmentPrice(Long id, Double price) {
+        // TODO: update any fields except the id field
         if (price < 5) return badRequest("Price should be bigger than 5!");
+
+        // TODO: business logic should be inside service layer
         Apartment apartment = apartmentService.findApartmentById(id);
         if (apartment == null) return badRequest("ID doesn't exist.");
         apartment.setPrice(price);
         Apartment updatedApartment = apartmentService.updateApartment(apartment);
+        // TODO: use Optional
+
         return ok(Json.toJson(updatedApartment));
     }
 
