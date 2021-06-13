fea801c689799f1cf2c575b473f49d860446c596
Junhyung Ki <kijoonh91@gmail.com>
2021-06-13 23:00:48 +0200

    appApartment, updateApartment modified but not tested

--- helloWorld/app/controllers/ApartmentRestController.java
+++ helloWorld/app/controllers/ApartmentRestController.java
@@ -4,6 +4,7 @@ import dto.ApartmentRequest;
 import models.Apartment;
 import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import services.ApartmentService;
 
@@ -31,11 +32,11 @@ public class ApartmentRestController extends Controller {
         return ok(Json.toJson(apartment));
     }
 
-    public Result addApartment(String name, String category, String description, Integer size, Double price) {
-        Result validateResult = validateApartmentRequest(category, size, price);
+    public Result addApartment(Http.Request request) {
+        Apartment requestedApartment = request.body().as(Apartment.class);
+        Result validateResult = validateApartmentRequest(requestedApartment);
         if (validateResult != null) return validateResult;
-        ApartmentRequest request = new ApartmentRequest(name, category, description, size, price);
-        Apartment apartment = apartmentService.saveApartment(request);
+        Apartment apartment = requestApartment(requestedApartment);
         return ok(Json.toJson(apartment));
     }
 
@@ -45,29 +46,37 @@ public class ApartmentRestController extends Controller {
         return ok(Json.toJson(apartment));
     }
 
-    public Result updateApartmentPrice(Long id, Double price) {
-        // TODO: update any fields except the id field
-        if (price < 5) return badRequest("Price should be bigger than 5!");
-
-        // TODO: business logic should be inside service layer
-        Apartment apartment = apartmentService.findApartmentById(id);
-        if (apartment == null) return badRequest("ID doesn't exist.");
-        apartment.setPrice(price);
-        Apartment updatedApartment = apartmentService.updateApartment(apartment);
-        // TODO: use Optional
-
+    public Result updateApartment(Http.Request request) {
+        Apartment requestedApartment = request.body().as(Apartment.class);
+        Result validateResult = validateApartmentUpdate(requestedApartment);
+        if (validateResult != null) return validateResult;
+        Apartment updatedApartment = apartmentService.updateApartment(requestedApartment);
         return ok(Json.toJson(updatedApartment));
     }
 
-    public Result validateApartmentRequest(String category, Integer size, Double price) {
-        if (!category.equals("A") && !category.equals("B") && !category.equals("C")) {
+    public Result validateApartmentRequest(Apartment apartment) {
+        String category = apartment.getCategory();
+        Integer size = apartment.getSize();
+        Double price = apartment.getPrice();
+        if (category == null || !category.equals("A") && !category.equals("B") && !category.equals("C")) {
             return badRequest("Category should either be \"A\" or \"B\" or \"C\"!");
-        } else if (size < 10) {
+        } else if (size == null || size < 10) {
             return badRequest("Size should not be smaller than 10!");
-        } else if (price < 5) {
+        } else if (price == null || price < 5) {
             return badRequest("Price should not be smaller than 5!");
         }
         return null;
+    }
+
+    public Result validateApartmentUpdate(Apartment requestedApartment) {
+        if (requestedApartment.getId() == null) return badRequest("ID must be given!");
+        return validateApartmentRequest(requestedApartment);
+    }
+
+    public Apartment requestApartment(Apartment requestedApartment) {
+        ApartmentRequest request = new ApartmentRequest(requestedApartment.getName(), requestedApartment.getCategory(),
+                requestedApartment.getDescription(), requestedApartment.getSize(), requestedApartment.getPrice());
+        return apartmentService.saveApartment(request);
     }
 
 }
